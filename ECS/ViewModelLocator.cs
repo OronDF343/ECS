703dd4326425443a1eb5ab73a45c3deddd702a26
OronDF343 <orondf343@gmail.com>
2016-10-25 15:34:20 +0300

    MVVM step 2

---
+++ ECS/ViewModelLocator.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using JetBrains.Annotations;
+using Microsoft.Practices.ServiceLocation;
+
+namespace ECS
+{
+    /// <summary>
+    /// Locates all ViewModels in the app.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<DesignerViewModel>();
+        }
+
+        /// <summary>
+        /// Gets the Designer property.
+        /// </summary>
+        [NotNull]
+        // ReSharper disable once MemberCanBeMadeStatic.Global
+        public DesignerViewModel Designer => ServiceLocator.Current.GetInstance<DesignerViewModel>();
+
+        /// <summary>
+        /// Cleans up all the resources.
+        /// </summary>
+        public static void Cleanup()
+        {
+        }
+    }
+}
