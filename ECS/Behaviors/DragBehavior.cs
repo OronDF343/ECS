fe8e9599f75b10796433099b268be028002ff7b8
OronDF343 <orondf343@gmail.com>
2016-10-30 15:16:09 +0200

    Added designer item and fix for interactivity in designer

---
+++ ECS/Behaviors/DragBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+using ECS.Controls;
+
+namespace ECS.Behaviors
+{
+    public class DragBehavior : Behavior<DesignerItem>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.DragDelta += AssociatedObject_DragDelta; 
+            base.OnAttached();
+        }
+
+        private void AssociatedObject_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            if (e == null) return;
+
+            var left = Canvas.GetLeft(AssociatedObject);
+            var top = Canvas.GetTop(AssociatedObject);
+            if (double.IsNaN(left)) left = 0;
+            if (double.IsNaN(top)) top = 0;
+            Canvas.SetLeft(AssociatedObject, Math.Max(left + e.HorizontalChange, 0));
+            Canvas.SetTop(AssociatedObject, Math.Max(top + e.VerticalChange, 0));
+            // TODO: Prevent OOB at bottom/right of canvas?
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.DragDelta -= AssociatedObject_DragDelta;
+            base.OnDetaching();
+        }
+    }
+}
---
+++ ECS/Controls/DesignerItem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ECS.Controls
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ECS.Controls"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ECS.Controls;assembly=ECS.Controls"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:DesignerItem/>
+    ///
+    /// </summary>
+    [ContentProperty(nameof(Content))]
+    public class DesignerItem : Thumb
+    {
+        static DesignerItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DesignerItem), new FrameworkPropertyMetadata(typeof(DesignerItem)));
+        }
+
+        /// <summary>
+        /// Registers a dependency property as backing store for the Content property
+        /// </summary>
+        public static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register(nameof(Content), typeof(object), typeof(DesignerItem),
+            new FrameworkPropertyMetadata(null,
+                  FrameworkPropertyMetadataOptions.AffectsRender |
+                  FrameworkPropertyMetadataOptions.AffectsParentMeasure));
+
+        /// <summary>
+        /// Gets or sets the Content.
+        /// </summary>
+        /// <value>The Content.</value>
+        public object Content
+        {
+            get { return GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+    }
+}
