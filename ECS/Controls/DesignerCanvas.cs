66dcfcbc0e45394dea67c11358c5691891232ad9
mor <morsofer9@gmail.com>
2016-11-15 14:22:27 +0200

    Added our Canvas

---
+++ ECS/Controls/DesignerCanvas.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ECS.Controls
+{
+    public class DesignerCanvas : Canvas 
+    {
+        protected override Size MeasureOverride(Size constraint)
+        {
+            var size = new Size();
+            foreach (UIElement element in Children)
+            {
+                var left = GetLeft(element);
+                var top = GetTop(element);
+                left = double.IsNaN(left) ? 0 : left;
+                top = double.IsNaN(top) ? 0 : top;
+
+                //measure desired size for each child
+                element.Measure(constraint);
+
+                var desiredSize = element.DesiredSize;
+                if (!double.IsNaN(desiredSize.Width) && !double.IsNaN(desiredSize.Height))
+                {
+                    size.Width = Math.Max(size.Width, left + desiredSize.Width);
+                    size.Height = Math.Max(size.Height, top + desiredSize.Height);
+                }
+            }
+            //for aesthetic reasons add extra points
+            //size.Width += 10;
+            //size.Height += 10;
+            return size;
+        }
+    }
+}
