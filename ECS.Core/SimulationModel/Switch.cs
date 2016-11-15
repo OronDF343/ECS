e241431ede2867e3c7a79eadac7caadb12083b66
mor <morsofer9@gmail.com>
2016-11-15 10:41:59 +0200

    Added switch to the model

---
+++ ECS.Core/SimulationModel/Switch.cs
@@ -0,0 +1,9 @@
+﻿namespace ECS.Core.SimulationModel
+{
+    public class Switch : Component
+    {
+        public bool IsClosed { get; set; }
+
+        public Switch(int id) : base(id) { }
+    }
+}
---
+++ ECS.Model/Switch.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace ECS.Model
+{
+    public class Switch : Component
+    {
+        private bool _isClosed;
+
+        [XmlAttribute]
+        public bool IsClosed
+        {
+            get { return _isClosed; }
+            set
+            {
+                _isClosed = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
