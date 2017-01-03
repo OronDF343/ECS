f2b6cf9c0f03e6d4a478c06ec66ddd9ecedf9a63
OronDF343 <orondf343@gmail.com>
2017-01-03 15:29:43 +0200

    Fixed colors!

--- ECS/Converters/CurrentToColorConverter.cs
+++ ECS/Converters/CurrentToColorConverter.cs
@@ -10,25 +10,28 @@ namespace ECS.Converters
         public double MinValue { get; set; }
         public double MaxValue { get; set; }
         public Color MinColor { get; set; }
+        public Color MidColor { get; set; }
         public Color MaxColor { get; set; }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var v = value as double? ?? 0.0;
+            if (double.IsNaN(v)) v = 0.0;
             v = Math.Max(v, MinValue);
             v = Math.Min(v, MaxValue);
             var factor = (v - MinValue) / (MaxValue - MinValue);
             return new SolidColorBrush(new Color
-                                       {
-                                           R = GenColorValue(MinColor.R, MaxColor.R, factor),
-                                           G = GenColorValue(MinColor.G, MaxColor.G, factor),
-                                           B = GenColorValue(MinColor.B, MaxColor.B, factor)
-                                       });
+            {
+                A = 255,
+                R = GenColorValue(MinColor.R, MidColor.R, MaxColor.R, factor),
+                G = GenColorValue(MinColor.G, MidColor.G, MaxColor.G, factor),
+                B = GenColorValue(MinColor.B, MidColor.B, MaxColor.B, factor)
+            });
         }
 
-        private byte GenColorValue(byte min, byte max, double factor)
+        private byte GenColorValue(byte min, byte mid, byte max, double factor)
         {
-            return (byte)(min + (max - min) * factor);
+            return factor > 0.5 ? (byte)(mid + (max - mid) * factor * 2) : (byte)(min + (mid - min) * factor * 2);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
