8118ad0d34161b0bf1079dc0b137c3d14e8b8d2d
OronDF343 <orondf343@gmail.com>
2017-04-19 17:06:49 +0300

    Remove extra backslash in script

--- fixwpfdesigner2017.bat
+++ fixwpfdesigner2017.bat
@@ -1,5 +1,5 @@
 @echo off
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-cd "%ProgramFiles(x86)%\\Microsoft Visual Studio\2017\Community\Common7\IDE\PrivateAssemblies"
+cd "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\Common7\IDE\PrivateAssemblies"
 gacutil -i System.Windows.Interactivity.dll
 pause
