ac76e33d870cd38886d380474d5a65a2b62c0b70
OronDF343 <orondf343@gmail.com>
2017-04-24 18:36:49 +0300

    Fixed result window

--- ECS/ViewModel/ResultDiagramViewModel.cs
+++ ECS/ViewModel/ResultDiagramViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using ECS.Model;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
 using JetBrains.Annotations;
 
 namespace ECS.ViewModel
@@ -10,11 +12,23 @@ namespace ECS.ViewModel
     {
         public ResultDiagramViewModel([NotNull] IEnumerable<DiagramObject> diagram, string name)
         {
-            DiagramObjects = new ObservableCollection<DiagramObject>(diagram);
+            _diagram = diagram;
             Name = name;
         }
 
-        public ObservableCollection<DiagramObject> DiagramObjects { get; }
+        private readonly IEnumerable<DiagramObject> _diagram;
+
+        public ObservableCollection<DiagramObject> DiagramObjects { get; } = new ObservableCollection<DiagramObject>();
         public string Name { get; }
+
+        public ICommand LoadedCommand => new RelayCommand(OnLoaded);
+
+        private void OnLoaded()
+        {
+            foreach (var dobj in _diagram)
+            {
+                DiagramObjects.Add(dobj);
+            }
+        }
     }
 }
