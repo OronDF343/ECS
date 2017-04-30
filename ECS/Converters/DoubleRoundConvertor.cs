398f86a720ed6bdd946d2679c56899c627b62cf2
mor <morsofer9@gmail.com>
2017-04-30 13:59:19 +0300

    rounded numbers

---
+++ ECS/Converters/DoubleRoundConvertor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ECS.Converters
+{
+    public class DoubleRoundConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var v = value as double? ?? 0.0;
+            if (double.IsNaN(v)) v = 0.0;
+            return v.ToString("#.###");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
